dcfd98e5201394cffc6763ff8824ef85e37173a4
mushtaq <mushta9.3@gmail.com>
2020-01-13 12:26:29 +0300

    SunSet commit

---
+++ app/src/main/java/com/mushtaq/sunset/SunsetActivity.java
@@ -0,0 +1,16 @@
+package com.mushtaq.sunset;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+
+public class SunsetActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+
+        return SunsetFragment.newInstance();
+    }
+}
